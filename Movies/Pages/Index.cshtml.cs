a26b97eae4ace3dc14de8befe979f568ed96e4eb
Jack Walter <jb3walter@ksu.edu>
2020-05-06 23:30:05 -0500

    Linq search implemented

--- Movies/Pages/Index.cshtml.cs
+++ Movies/Pages/Index.cshtml.cs
@@ -73,11 +73,92 @@ namespace Movies.Pages
         /// <param name="RottenMax">The maximum Rotten Tomatoes Rating</param>
         public void OnPost()
         {
+            /*
             Movies = MovieDatabase.Search(SearchTerms);
             Movies = MovieDatabase.FilterByMPAARating(Movies, MPAARatings);
             Movies = MovieDatabase.FilterByGenre(Movies, Genres);
             Movies = MovieDatabase.FilterByIMDBRating(Movies, IMDBMin, IMDBMax);
             Movies = MovieDatabase.FilterByRottenTomatoesRating(Movies, RottenMin, RottenMax);
+            */
+
+            Movies = MovieDatabase.All;
+
+            // Search movie titles for the SearchTerms
+            if (SearchTerms != null)
+            {
+                Movies = Movies.Where(movie =>
+                    movie.Title != null &&
+                    movie.Title.Contains(SearchTerms, StringComparison.CurrentCultureIgnoreCase)
+                );
+            }
+            // Filter by MPAA Ratings
+            if (MPAARatings != null && MPAARatings.Length != 0)
+            {
+                Movies = Movies.Where(movie =>
+                    movie.MPAARating != null &&
+                    MPAARatings.Contains(movie.MPAARating)
+                );
+            }
+            // Filter by Genre 
+            if (Genres != null && Genres.Count() != 0)
+            {
+                Movies = Movies.Where(movie =>
+                    movie.MajorGenre != null &&
+                    Genres.Contains(movie.MajorGenre)
+                );
+            }
+            // Filter by IMBD Rating
+            if (IMDBMax != null || IMDBMin != null)
+            {
+                // Min is null
+                if (IMDBMin == null)
+                {
+                    Movies = Movies.Where(movie =>
+                        movie.IMDBRating <= IMDBMax
+                    );
+                }
+                // Max is null
+                if (IMDBMax == null)
+                {
+                    Movies = Movies.Where(movie =>
+                        movie.IMDBRating >= IMDBMin
+                    );
+                }
+                // Both are specified
+                if (IMDBMax != null && IMDBMin != null)
+                {
+                    Movies = Movies.Where(movie =>
+                        movie.IMDBRating >= IMDBMin &&
+                        movie.IMDBRating <= IMDBMax
+                    );
+                }
+            }
+            // Filter by RottenTomatoes Rating
+            if (RottenMax != null || RottenMin != null)
+            {
+                // Min is null
+                if (RottenMin == null)
+                {
+                    Movies = Movies.Where(movie =>
+                        movie.RottenTomatoesRating <= RottenMax
+                    );
+                }
+                // Max is null
+                if (RottenMax == null)
+                {
+                    Movies = Movies.Where(movie =>
+                        movie.RottenTomatoesRating >= RottenMin
+                    );
+                }
+                // Both are specified
+                if (RottenMax != null && RottenMin != null)
+                {
+                    Movies = Movies.Where(movie =>
+                        movie.RottenTomatoesRating >= RottenMin &&
+                        movie.RottenTomatoesRating <= RottenMax
+                    );
+                }
+            }
         }
     }
 }
